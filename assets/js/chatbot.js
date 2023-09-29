9adcfe956bf69e8aedadfcc670579300a5c899a4
Your Name <simon.be47@gmail.com>
2023-09-29 14:06:59 +0200

    chatbot html, scss and openner/closer behaviour whith js

--- assets/js/chatbot.js
+++ assets/js/chatbot.js
@@ -8,20 +8,40 @@ document.addEventListener("DOMContentLoaded", () => {
     const chatContainer = document.getElementById("chatContainer")
     const chatbotHeader = document.getElementById("chatbotHeader")
     const chatbotCloser = document.getElementById("chatbotCloser")
+    const chatInput = document.getElementById("chatInput")
 
     chatbotOpenner.addEventListener("click", () => {
-        chatbotContainer.style.border = "solid 2px #333"
-        chatbotContainer.style.left = "0"
-        chatbotContainer.style.right = "0"
-        chatbotContainer.style.width = "300px"
+        chatbotContainer.classList.toggle("open")
         chatbotCloser.style.display = "block"
-        chatbotHeader.style.borderBottom = "solid 2px #333"
-        chatbotContainer.style.background = "white"
         chatbotOpenner.classList.toggle("hide")
         chatIconContainer.classList.toggle("hide")
         helloChat.classList.toggle("hide")
         robotIcon.classList.toggle("hide")
         chatWithMe.classList.toggle("hide")
         chatContainer.classList.toggle("hide")
+        chatbotHeader.style.borderBottom = "solid 2px #333"
+        chatInput.focus()
     })
+
+    chatbotCloser.addEventListener("click", () => {
+        closeChatbot()
+    })
+
+    document.addEventListener("click", (event) => {
+        if (chatbotContainer.classList.contains("open") && !chatbotContainer.contains(event.target)) {
+            closeChatbot()
+        }
+    })
+
+    function closeChatbot() {
+        chatbotContainer.classList.toggle("open")
+        chatbotCloser.style.display = "none"
+        chatbotHeader.style.borderBottom = "none"
+        chatbotOpenner.classList.toggle("hide")
+        chatIconContainer.classList.toggle("hide")
+        helloChat.classList.toggle("hide")
+        robotIcon.classList.toggle("hide")
+        chatWithMe.classList.toggle("hide")
+        chatContainer.classList.toggle("hide")
+    }
 })
