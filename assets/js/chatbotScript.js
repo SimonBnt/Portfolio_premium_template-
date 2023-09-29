d49d295654755d0524be4063a25ae0fc9d0b9666
Your Name <simon.be47@gmail.com>
2023-09-29 15:52:21 +0200

    chatbot script fix + chatbot fiel scss fix

--- assets/js/chatbotScript.js
+++ assets/js/chatbotScript.js
@@ -1,47 +1,51 @@
 document.addEventListener("DOMContentLoaded", () => {
     const chatField = document.getElementById("chatField")
 
-    const utterances = [
-        ["how are you","how is life","how are things"],
-        ["hi","hey","hello","good morning","good afternoon"],
-        ["what are you doing","what is going on","what is up"],
-        ["how old are you"],
-        ["who are you","are you human","are you bot","are you human or bot"],
-        ["who created you","who made you"],
-        ["your name please","your name","may i know your name","what is your name","what call yourself"],
-        ["happy","good","fun","wonderful","fantastic","cool"],
-        ["bad","bored","tired"],
-        ["help me","tell me story","tell me joke"],
-        ["ah","yes","ok","okay","nice"],
-        ["bye","good bye","goodbye","see you later"],
-        ["what should i eat today"],
-        ["what","why","how","where","when"],
-        ["no","not sure","maybe","no thanks"],
-        [""],
-        ["haha","ha","lol","hehe","funny","joke"]
+    const question = [
+        ["how are you","how is life","how are things"], // -- 1 -- //  
+        ["hi","hey","hello","good morning","good afternoon"],   // -- 2 -- //
+        ["what are you doing","what is going on","what is up"], // -- 3 -- //
+        ["how old are you"],    // -- 4 -- //
+        ["who are you","are you human","are you bot","are you human or bot"],   // -- 5 -- //
+        ["who created you","who made you"], // -- 6 -- //
+        ["your name please","your name","may i know your name","what is your name","what call yourself"],   // -- 7 -- //
+        ["happy","good","fun","wonderful","fantastic","cool"],  // -- 8 -- //
+        ["bad","bored","tired"],    // -- 9 -- //
+        ["help me","tell me story","tell me joke"], // -- 10 -- //
+        ["ah","yes","ok","okay","nice"],    // -- 11 -- //
+        ["bye","good bye","goodbye","see you later"],   // -- 12 -- //
+        ["what should i eat today"],    // -- 13 -- //
+        ["what","why","how","where","when"],    // -- 14 -- //
+        ["no","not sure","maybe","no thanks"],  // -- 15 -- //
+        [""],   // -- 16 -- //
+        ["haha","ha","lol","hehe","funny","joke"],   // -- 17 -- //
+        ["do you eat the mulet"],   // -- 18 -- //
+        ["do you eat your epinard gros connard"]   // -- 19 -- //
     ]
 
     const answers = [
-        ["Fine... how are you?","Pretty well, how are you?","Fantastic, how are you?"],
-        ["Hello!","Hi!","Hey!","Hi there!","Howdy"],
-        ["Nothing much","About to go to sleep","Can you guess?","I don't know actually"],
-        ["I am infinite"],
-        ["I am just a bot","I am a bot. What are you?"],
-        ["The one true God, JavaScript"],
-        ["I am nameless","I don't have a name"],
-        ["Have you ever felt bad?","Glad to hear it"],
-        ["Why?","Why? You shouldn't!"],
-        ["What about?","Once upon a time..."],
-        ["Tell me a story","Tell me a joke","Tell me about yourself"],
-        ["Bye","Goodbye","See you later"],
-        ["Pasta","Burger"],
-        ["Great question"],
-        ["That's ok","What do you want to talk about?"],
-        ["Please say something :("],
-        ["Haha!","Good one!"]
+        ["Fine... how are you?","Pretty well, how are you?","Fantastic, how are you?"], // -- 1 -- //
+        ["Hello!","Hi!","Hey!","Hi there!","Howdy"],    // -- 2 -- //
+        ["Nothing much","About to go to sleep","Can you guess?","I don't know actually"],   // -- 3 -- //
+        ["I am infinite"],  // -- 4 -- //
+        ["I am just a bot","I am a bot. What are you?"],    // -- 5 -- //
+        ["The one true God, JavaScript"],   // -- 6 -- //
+        ["I am nameless","I don't have a name"],    // -- 7 -- //
+        ["Have you ever felt bad?","Glad to hear it"],  // -- 8 -- //
+        ["Why?","Why? You shouldn't!"], // -- 9 -- //
+        ["What about?","Once upon a time..."],  // -- 10 -- //
+        ["Tell me a story","Tell me a joke","Tell me about yourself"],  // -- 11 -- //
+        ["Bye","Goodbye","See you later"],  // -- 12 -- //
+        ["Pasta","Burger"], // -- 13 -- //
+        ["Great question"], // -- 14 -- //
+        ["That's ok","What do you want to talk about?"],    // -- 15 -- //
+        ["Please say something :("],    // -- 16 -- //
+        ["Haha!","Good one!"],   // -- 17 -- //
+        ["Yes I do","Of course it's delicious :)","Yes sometimes, but do you eat your épinard gros connard?"],   // -- 18 -- //
+        ["Yes I do","Of course it's delicious :)"]   // -- 19 -- //
     ]
 
-    const alternatives = ["Go on...","Try again"]
+    const alternatives = ["Go on...","Try again","I don't understand","Can you repeat the question ?"]
 
     chatInput.addEventListener("keydown", (e) => {
         if (e.code === "Enter") {
@@ -61,9 +65,8 @@ document.addEventListener("DOMContentLoaded", () => {
         .replace(/ please/g, "")
         .replace(/r u/g, "are you")
     
-        if (compare(utterances, answers, text)) {
-            // Search for exact match in triggers
-            product = compare(utterances, answers, text)
+        if (compare(question, answers, text)) {
+            product = compare(question, answers, text)
         } else {
             product = alternatives[Math.floor(Math.random() * alternatives.length)]
         }
@@ -71,13 +74,13 @@ document.addEventListener("DOMContentLoaded", () => {
         addChatEntry(input, product)
     }
     
-    function compare(utterancesArray, answersArray, string) {
+    function compare(questionArray, answersArray, string) {
         let reply
         let replyFound = false
 
-        for (let x = 0; x < utterancesArray.length; x++) {
-            for (let y = 0; y < utterancesArray[x].length; y++) {
-                if (utterancesArray[x][y] === string) {
+        for (let x = 0; x < questionArray.length; x++) {
+            for (let y = 0; y < questionArray[x].length; y++) {
+                if (questionArray[x][y] === string) {
                     let replies = answersArray[x]
                     reply = replies[Math.floor(Math.random() * replies.length)]
                     replyFound = true
